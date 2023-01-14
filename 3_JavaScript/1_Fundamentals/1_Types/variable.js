4460681091fac847a8cf1b2dad232184dc28e2bd
Jaimin Prajapati <enthralall@gmail.com>
2023-01-14 13:31:00 +0530

    JS Fundamentals - Types

---
+++ 3_JavaScript/1_Fundamentals/1_Types/variable.js
@@ -0,0 +1,40 @@
+/** JS Datatypes **/
+
+// Number
+let a = 1;
+console.log(a, typeof a);
+
+// String
+let b = 'Hello';
+console.log(b, typeof b);
+
+// Big Integer - BigInt
+let c = BigInt(12e54);
+console.log(c, typeof c);
+
+// Boolean
+let d = true;
+let d1 = false;
+console.log(d, typeof d, d1, typeof d1);
+
+// Undefined
+let e = undefined;
+console.log(e, typeof e);
+
+// Null
+let f = null;
+console.log(f, typeof f);
+
+// Object
+let g = {
+    name: 'Jaimin'
+};
+console.log(g, typeof g);
+
+// Array
+let h = [1, 2, 3, 4, 5];
+console.log(h, typeof h);
+
+// Symbol
+let i = Symbol('I am Atomic');
+console.log(i, typeof i);
