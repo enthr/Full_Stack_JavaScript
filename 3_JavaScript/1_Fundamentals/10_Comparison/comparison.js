23448fd583611203f99a9780aa7081b57f1b45fe
Jaimin Prajapati <enthralall@gmail.com>
2023-01-14 14:58:11 +0530

    JS Fundamentals - Comparison

---
+++ 3_JavaScript/1_Fundamentals/10_Comparison/comparison.js
@@ -0,0 +1,16 @@
+console.log(4 > 3); // true
+console.log(4 >= 3); // true
+console.log(4 < 3); // false
+console.log(4 <= 3); // false
+console.log(4 == 4); // true
+console.log(4 === 4); // true
+console.log(4 != 4); // false
+console.log(4 !== 4); // false
+console.log(4 != '4'); // false
+console.log(4 == '4'); // true
+console.log(4 === '4'); // false
+
+const a = 'Python';
+const b = 'Jargon';
+
+console.log(a, a.length, b, b.length, a.length == `${b.length}`);
