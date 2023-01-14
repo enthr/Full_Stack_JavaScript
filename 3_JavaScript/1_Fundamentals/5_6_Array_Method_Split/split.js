b05f2e159fa482413e2d002b35bad827b17a7f0f
Jaimin Prajapati <enthralall@gmail.com>
2023-01-14 14:01:39 +0530

    JS Fundamentals - Array Method split

---
+++ 3_JavaScript/1_Fundamentals/5_6_Array_Method_Split/split.js
@@ -0,0 +1,11 @@
+const a = 'Apple, Banana, Citrus, Dates, Eggplant, Figs';
+const b = a.split(', ');
+
+console.log(a, typeof a);
+console.log(b, typeof b);
+
+const companies = 'Facebook, Google, Microsoft, Apple, IBM, Oracle, Amazon';
+const companies1 = companies.split(', ');
+
+console.log(companies, typeof companies);
+console.log(companies1, typeof companies1);
--- 3_JavaScript/1_Fundamentals/5_Array_Method_Split/split.js
+++
@@ -1,5 +0,0 @@
-const a = 'Apple, Banana, Citrus, Dates, Eggplant, Figs';
-const b = a.split(', ');
-
-console.log(a, typeof a);
-console.log(b, typeof b);
