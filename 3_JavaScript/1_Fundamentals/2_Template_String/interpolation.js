3cc9c046d14f5d9d101b5841c9f68a44f8e12489
Jaimin Prajapati <enthralall@gmail.com>
2023-01-14 13:40:45 +0530

    JS Fundamentals - Template Strings

---
+++ 3_JavaScript/1_Fundamentals/2_Template_String/interpolation.js
@@ -0,0 +1,10 @@
+const profile = {
+    firstName: "Jaimin",
+    lastName: "Prajapati",
+    relationShipStatus: 'Non of Your Business',
+    country: 'Bharat',
+    age: 20
+}
+
+console.log(`Hello, I am ${profile.firstName} ${profile.lastName}.`);
+console.log(`My Realtionship is ${profile.relationShipStatus}. I live in ${profile.country}. I am ${profile.age} old.`);
