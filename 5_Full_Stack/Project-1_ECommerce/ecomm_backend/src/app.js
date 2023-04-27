d07c9faef6a1b4066d6c52afd1f435b3216fe57f
Jaimin Prajapati <enthralall@gmail.com>
2023-04-27 23:49:53 +0530

    Full Stack Project-1 app error handling moved

--- 5_Full_Stack/Project-1_ECommerce/ecomm_backend/src/app.js
+++ 5_Full_Stack/Project-1_ECommerce/ecomm_backend/src/app.js
@@ -4,4 +4,43 @@ const app = fastify({
     logger: (process.env.NODE_ENV === "dev") ? true : false
 });
 
+app.setErrorHandler((error, request, reply) => {
+    // Handle MongoDB Errors
+    if (error.name === "MongoError") {
+        // console.error("MongoDB Error: ", error);
+        app.log.error("MongoDB Error: ", error);
+        reply.code(500).send({ message: "Internal Server Error" });
+        return;
+    }
+
+    // Handle Validation Errors
+    if (error.name === "ValidationError") {
+        // console.error("Validation Error: ", error);
+        app.log.error("Validation Error: ", error);
+        reply.code(400).send({ message: error.message });
+        return;
+    }
+
+    // Duplicate Key Error
+    if (error.name === "DuplicateKeyError") {
+        // console.error("Duplicate Key Error: ", error);
+        app.log.error("Duplicate Key Error: ", error);
+        reply.code(409).send({ message: "Already Exists" });
+        return;
+    }
+
+    // Docuemnt Not Found
+    if (error.name === "DocumentNotFoundError") {
+        // console.error("Document Not Found: ", error);
+        app.log.error("Document Not Found: ", error);
+        reply.code(404).send({ message: "Not Found" });
+        return;
+    }
+
+    // Handle Other Errors
+    // console.error("ERROR: ", error);
+    app.log.error("ERROR: ", error);
+    reply.code(500).send({ message: "Internal Server Error" });
+});
+
 export default app;
--- 5_Full_Stack/Project-1_ECommerce/ecomm_backend/src/index.js
+++ 5_Full_Stack/Project-1_ECommerce/ecomm_backend/src/index.js
@@ -12,45 +12,6 @@ const MONGODB_URI = process.env.MONGODB_URI;
 console.log(typeof PORT, PORT);
 console.log(typeof MONGODB_URI, MONGODB_URI);
 
-app.setErrorHandler((error, request, reply) => {
-    // Handle MongoDB Errors
-    if (error.name === "MongoError") {
-        // console.error("MongoDB Error: ", error);
-        app.log.error("MongoDB Error: ", error);
-        reply.code(500).send({ message: "Internal Server Error" });
-        return;
-    }
-
-    // Handle Validation Errors
-    if (error.name === "ValidationError") {
-        // console.error("Validation Error: ", error);
-        app.log.error("Validation Error: ", error);
-        reply.code(400).send({ message: error.message });
-        return;
-    }
-
-    // Duplicate Key Error
-    if (error.name === "DuplicateKeyError") {
-        // console.error("Duplicate Key Error: ", error);
-        app.log.error("Duplicate Key Error: ", error);
-        reply.code(409).send({ message: "Already Exists" });
-        return;
-    }
-
-    // Docuemnt Not Found
-    if (error.name === "DocumentNotFoundError") {
-        // console.error("Document Not Found: ", error);
-        app.log.error("Document Not Found: ", error);
-        reply.code(404).send({ message: "Not Found" });
-        return;
-    }
-
-    // Handle Other Errors
-    // console.error("ERROR: ", error);
-    app.log.error("ERROR: ", error);
-    reply.code(500).send({ message: "Internal Server Error" });
-});
-
 const server = async () => {
     try {
         await mongoose.connect(MONGODB_URI);
