67ecc490a2df82848a575e986996f98b80c390c8
Jaimin Prajapati <enthralall@gmail.com>
2023-04-27 23:13:19 +0530

    Full Stack Project-1 userSchema Created

--- 5_Full_Stack/Project-1_ECommerce/ecomm_backend/src/models/user.schema.js
+++ 5_Full_Stack/Project-1_ECommerce/ecomm_backend/src/models/user.schema.js
@@ -1,15 +1,32 @@
+import util from "util";
+import crypto from "crypto";
 import mongoose from "mongoose";
 import isEmail from "validator/es/lib/isEmail";
+import isURL from "validator/es/lib/isURL";
 import * as argon2 from "argon2";
+import JWT from "jsonwebtoken";
 
+import app from "../app";
 import authRoles from "../utils/authRoles";
+
+const generateJWTAsync = util.promisify(JWT.sign);
+const generateForgotTokenAsync = util.promisify(crypto.randomBytes);
 
 const userSchema = new mongoose.Schema({
     name: {
         type: String,
         trim: true,
-        maxLength: [50, "Name Must Be less than 50 characters"],
         required: [true, "Name is Required"]
+    },
+    imgUrl: {
+        type: String,
+        trim: true,
+        validate: {
+            validator: function (value) {
+                return isURL(value);
+            },
+            message: "Collection Image URL is Invalid"
+        }
     },
     email: {
         type: String,
@@ -23,6 +40,12 @@ const userSchema = new mongoose.Schema({
             },
             message: "Email is Invalid"
         }
+    },
+    phone: {
+        type: String,
+        trim: true,
+        required: [true, "Phone is Required"],
+        maxLength: [50, "Phone Must Be Less Than 15 Characters"]
     },
     password: {
         type: String,
@@ -41,6 +64,14 @@ const userSchema = new mongoose.Schema({
         default: false,
         select: false
     },
+    verificationToken: {
+        type: String,
+        select: false
+    },
+    verificationExpire: {
+        type: Date,
+        select: false
+    },
     isBlocked: {
         type: Boolean,
         default: false,
@@ -53,7 +84,7 @@ const userSchema = new mongoose.Schema({
     forgotPasswordExpire: {
         type: Date,
         select: false
-    }    
+    }
 }, { timestamps: true });
 
 // Encrypt Password Before Saving
@@ -63,10 +94,11 @@ userSchema.pre("save", async function (next) {
     }
     try {
         this.password = await argon2.hash(this.password);
+        return next();
     } catch (error) {
-        return next(error);
+        app.log.error(error);
+        return next(new Error("Password Encryption Failed"));
     }
-    return next();
 });
 
 userSchema.methods = {
@@ -74,6 +106,89 @@ userSchema.methods = {
     comparePassword: async function (enteredPassword) {
         return await argon2.verify(this.password, enteredPassword);
     },
-}
+
+    // Generate JWT
+    generateJWT: async function () {
+        const payload = {
+            _id: this._id,
+            name: this.name,
+            email: this.email,
+            role: this.role
+        };
+        const options = {
+            expiresIn: process.env.JWT_EXPIRE,
+            algorithm: "RS256"
+        };
+        try {
+            const token = await generateJWTAsync(payload, process.env.JWT_SECRET, options);
+            return token;
+        } catch (error) {
+            app.log.error(error);
+            throw new Error("JWT Generation Failed");
+        }
+    },
+
+    // Generate Forgot Password Token
+    generateForgotPasswordToken: async function () {
+        try {
+            const resetToken = (await generateForgotTokenAsync(20)).toString("hex");
+
+            // Store Hashed Token
+            const hashToken = await argon2.hash(resetToken);
+
+            this.forgotPasswordToken = hashToken;
+
+            // Time for Token Expiration
+            this.forgotPasswordExpire = Date.now() + 20 * 60 * 1000;
+
+            return resetToken;
+        } catch (error) {
+            app.log.error(error);
+            throw new Error("Forgot Password Token Generation Failed");
+        }
+    },
+
+    // Verify Forgot Password Token
+    verifyForgotPasswordToken: async function (resetToken) {
+        try {
+            const isMatch = await argon2.verify(this.forgotPasswordToken, resetToken);
+            return isMatch;
+        } catch (error) {
+            app.log.error(error);
+            throw new Error("Forgot Password Token Verification Failed");
+        }
+    },
+
+    // Generate Email Verification Token
+    generateEmailVerificationToken: async function () {
+        try {
+            const verificationToken = (await generateForgotTokenAsync(20)).toString("hex");
+
+            // Store Hashed Token
+            const hashToken = await argon2.hash(verificationToken);
+
+            this.verificationToken = hashToken;
+
+            // Time for Token Expiration
+            this.verificationExpire = Date.now() + 20 * 60 * 1000;
+
+            return verificationToken;
+        } catch (error) {
+            app.log.error(error);
+            throw new Error("Email Verification Token Generation Failed");
+        }
+    },
+
+    // Verify Email Verification Token
+    verifyEmailVerificationToken: async function (verificationToken) {
+        try {
+            const isMatch = await argon2.verify(this.verificationToken, verificationToken);
+            return isMatch;
+        } catch (error) {
+            app.log.error(error);
+            throw new Error("Email Verification Token Verification Failed");
+        }
+    }
+};
 
 export default mongoose.model("User", userSchema);
