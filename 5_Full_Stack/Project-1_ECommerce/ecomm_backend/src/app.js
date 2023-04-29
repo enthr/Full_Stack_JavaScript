f8b74882ce3bdeeaa4d6576542b0a2c11a6b9b19
Jaimin Prajapati <enthralall@gmail.com>
2023-04-29 12:56:45 +0530

    Full Stack Project-1 Moved env Variables to config/index.js

--- 5_Full_Stack/Project-1_ECommerce/ecomm_backend/src/app.js
+++ 5_Full_Stack/Project-1_ECommerce/ecomm_backend/src/app.js
@@ -1,46 +1,98 @@
 import fastify from "fastify";
+import sensible from "@fastify/sensible";
+import cookie from "@fastify/cookie";
+import cors from "@fastify/cors";
+import formbody from "@fastify/formbody";
+import sjson from "secure-json-parse";
+import qs from "qs";
+
+import CustomError from "./utils/CustomError.js";
+import config from "./config";
 
 const app = fastify({
-    logger: (process.env.NODE_ENV === "dev") ? true : false
+    logger: (config.NODE_ENV === "DEV") ? true : false
 });
 
-app.setErrorHandler((error, request, reply) => {
-    // Handle MongoDB Errors
-    if (error.name === "MongoError") {
-        // console.error("MongoDB Error: ", error);
-        app.log.error("MongoDB Error: ", error);
-        reply.code(500).send({ message: "Internal Server Error" });
+app.register(sensible);
+app.register(cookie, {
+    secret: config.COOKIE_SIGN,
+    parseOptions: {
+        secure: (config.NODE_ENV === "DEV") ? false : true,
+        signed: true,
+        sameSite: "strict",
+        httpOnly: true,
+        path: "/",
+        expires: new Date(Date.now() + 3 * 24 * 60 * 60 * 1000),
+        domain: config.DOMAIN
+    }
+});
+app.register(cors, {
+    origin: (config.NODE_ENV === "DEV") ? "*" : config.ALLOWED_ORIGINS,
+    credentials: true,
+    allowedHeaders: ["Content-Type", "Authorization"],
+    methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
+    preflightContinue: true,
+    optionsSuccessStatus: 200,
+    maxAge: 86400,
+    exposedHeaders: ["Content-Type", "Authorization"],
+    preflight: true,
+    hideOptionsRoute: true,
+    strictPreflight: true
+});
+app.register(formbody, { parser: async (str) => qs.parse(str) });
+app.addContentTypeParser("application/json", { parseAs: "string" }, async (_request, body, done) => {
+    try {
+        const json = await sjson.parse(body, { protoAction: "error", constructorAction: "error" });
+        done(null, json);
+    } catch (error) {
+        done(new CustomError("Bad Request", 400));
+    }
+});
+
+app.setErrorHandler(async (error, _request, reply) => {
+    try {
+        // Handle MongoDB Errors
+        if (error.name === "MongoError") {
+            app.log.error("MongoDB Error: ", error);
+            reply.code(error.code || 500).json({ success: false, message: "DB Error" });
+            return;
+        }
+
+        // Handle Validation Errors
+        if (error.name === "ValidationError") {
+            app.log.error("Validation Error: ", error);
+            reply.code(error.code || 400).json({ success: false, message: "Incorrect Information" });
+            return;
+        }
+
+        // Duplicate Key Error
+        if (error.name === "DuplicateKeyError") {
+            app.log.error("Duplicate Key Error: ", error);
+            reply.code(error.code || 409).json({ success: false, message: "Already Exists" });
+            return;
+        }
+
+        // MongoDocuemnt Not Found
+        if (error.name === "DocumentNotFoundError") {
+            app.log.error("Document Not Found: ", error);
+            reply.code(error.code || 404).json({ success: false, message: "Info Not Found" });
+            return;
+        }
+
+        // Handle Other Errors
+        app.log.error("ERROR: ", error);
+        reply.code(error.code || 500).json({ success: false, message: error.message || "Internal Server Error" });
+        return;
+    } catch (error) {
+        app.log.error("ERROR: ", error);
+        reply.code(error.code || 500).json({ success: false, message: error.message || "Internal Server Error" });
         return;
     }
+});
 
-    // Handle Validation Errors
-    if (error.name === "ValidationError") {
-        // console.error("Validation Error: ", error);
-        app.log.error("Validation Error: ", error);
-        reply.code(400).send({ message: error.message });
-        return;
-    }
-
-    // Duplicate Key Error
-    if (error.name === "DuplicateKeyError") {
-        // console.error("Duplicate Key Error: ", error);
-        app.log.error("Duplicate Key Error: ", error);
-        reply.code(409).send({ message: "Already Exists" });
-        return;
-    }
-
-    // Docuemnt Not Found
-    if (error.name === "DocumentNotFoundError") {
-        // console.error("Document Not Found: ", error);
-        app.log.error("Document Not Found: ", error);
-        reply.code(404).send({ message: "Not Found" });
-        return;
-    }
-
-    // Handle Other Errors
-    // console.error("ERROR: ", error);
-    app.log.error("ERROR: ", error);
-    reply.code(500).send({ message: "Internal Server Error" });
+app.setNotFoundHandler(async (_request, reply) => {
+    reply.code(error.code || 404).json({ success: false, message: error.message || "Not Found" });
+    return;
 });
 
 export default app;
--- 5_Full_Stack/Project-1_ECommerce/ecomm_backend/src/index.js
+++ 5_Full_Stack/Project-1_ECommerce/ecomm_backend/src/index.js
@@ -1,26 +1,18 @@
 import mongoose from "mongoose";
-import dotenv from "dotenv";
 
+import config from "./config/index.js";
 import app from "./app.js";
-
-// Load Environment Variables
-dotenv.config();
-
-const PORT = parseInt(process.env.PORT);
-const MONGODB_URI = process.env.MONGODB_URI;
-
-console.log(typeof PORT, PORT);
-console.log(typeof MONGODB_URI, MONGODB_URI);
 
 const server = async () => {
     try {
-        await mongoose.connect(MONGODB_URI);
+        await mongoose.connect(config.MONGODB_URI);
         console.log("DB Connected");
         mongoose.connection.on("error", (err) => {
             console.error("DB Connection Error: ", err);
         });
-        app.listen({ port: PORT });
-        app.log.info(`Server Listening on PORT ${PORT}}`);
+
+        await app.listen({ port: config.PORT });
+        app.log.info(`Server Listening on PORT ${config.PORT}}`);
     } catch (err) {
         app.log.error(err);
         process.exit(1);
