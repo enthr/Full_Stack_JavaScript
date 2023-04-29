acb86816ea255e00dce7951063c937afbec41c8d
Jaimin Prajapati <enthralall@gmail.com>
2023-04-29 14:19:56 +0530

    Full Stack Project-1 login and logout auth controllers defined

--- 5_Full_Stack/Project-1_ECommerce/ecomm_backend/src/controllers/auth.controller.js
+++ 5_Full_Stack/Project-1_ECommerce/ecomm_backend/src/controllers/auth.controller.js
@@ -36,3 +36,52 @@ export const register = asyncHandler(async (request, reply) => {
     reply.setCookie("token", token);
     reply.code(200).json({ success: true, token: token, data: newUser });
 });
+
+/*
+* @LOGIN
+* @route: /api/v1/auth/login
+* @description: User Login Controller
+* @returns: User Object
+*/
+export const login = asyncHandler(async (request, reply) => {
+    const { email, password } = request.body;
+
+    if (!email || !password) {
+        throw new CustomError("Please Provide Email And Password", 400);
+    }
+
+    const user = await User.findOne({ email: email });
+
+    if (!user) {
+        throw new CustomError("Invalid Credentials", 401);
+    }
+
+    if (user.isBlocked) {
+        throw new CustomError("Your Account Has Been Blocked", 403);
+    }
+
+    if (!user.isVerified) {
+        throw new CustomError("Please Verify Your Email", 403);
+    }
+
+    const isPassMatch = await user.comparePassword(password);
+
+    if (isPassMatch) {
+        const token = await user.generateJWT();
+        reply.setCookie("token", token);
+        reply.code(200).json({ success: true, token: token, data: user });
+    }
+
+    throw new CustomError("Invalid Credentials", 401);
+});
+
+/*
+* @LOGOUT
+* @route: /api/v1/auth/logout
+* @description: User Logout Controller, Remove token from cookie
+* @returns: success
+*/
+export const logout = asyncHandler(async (_request, reply) => {
+    reply.clearCookie("token");
+    reply.code(200).json({ success: true, data: {} });
+});
